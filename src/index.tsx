2ef423aa11928587cfc677402621de617d99112d
gaduna <gabriel.aduna@bwdesigngroup.com>
2024-08-02 12:35:15 -0700

    Finally got compiled code to show up in Figma

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,6 @@ import App from './App';
 
 const container = document.getElementById('root');
 if (container) {
-    const root = createRoot(container); // createRoot(container!) if you use TypeScript
+    const root = createRoot(container);
     root.render(<App />);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = (env, argv) => ({
   mode: argv.mode === 'production' ? 'production' : 'development',
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   entry: {
-    code: './src/code.ts', // Entry point for plugin code
-    ui: './src/index.tsx', // Entry point for React UI
+    code: './src/code.ts',
+    ui: './src/index.tsx', 
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'babel-loader',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+          'ts-loader',
+        ],
         exclude: /node_modules/,
       },
       {
@@ -30,10 +40,18 @@ module.exports = (env, argv) => ({
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'styles.css',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/ui.html',
+      filename: 'ui.html',
+      inlineSource: '.(js)$',
+      chunks: ['ui'],
+    }),
+    new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
   ],
 });
